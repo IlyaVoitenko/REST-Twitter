c4bad953133c7dc710428447f881c219cc9e15f8
IlyaVoitenko <79865476+IlyaVoitenko@users.noreply.github.com>
2021-04-01 17:28:53 +0300

    added posts of users

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let selectLink = document.querySelectorAll('.liSelect')
-
+let containerNews = document.querySelector('#containerNews')
 function selectItem(elementList) {
     for (var i = 0; i < elementList.length; i++) {
         elementList[i].addEventListener("click", function(e) {
@@ -17,4 +17,39 @@ function selectItem(elementList) {
          };
 }
 selectItem(selectLink)
-   
+   fetch('https://jsonplaceholder.typicode.com/users')
+   .then(dataUsers => dataUsers.json())
+   .then(dataUsers =>{
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(dataPosts => dataPosts.json())
+        .then(dataPosts => {
+            console.log({dataUsers});
+            console.log({dataPosts});
+       dataUsers.forEach(user => {
+        containerNews.innerHTML+=`
+        <p></p>
+        <div class ="divInfo">
+        <p class="name">${user.name} ${user.username}  </p>`
+        dataPosts.forEach(post => {
+           if (user.id == post.userId) {
+            containerNews.innerHTML+=`
+            <div class="divInfo">
+            ${post.title}
+            </div>
+            `
+           }
+        });
+        containerNews.innerHTML+=`</div>`
+        })
+    //    console.log({dataUsers});
+    //    dataUsers.forEach(i => {
+    //     containerNews.innerHTML+=`
+    //     <div class ="divInfo">
+    //     <p>${i.name} ${i.username}</p>`
+        
+    //     containerNews.innerHTML+=`</div>`
+       // <div class='containerTitle'></div>
+        
+        
+       });
+   })
